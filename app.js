91002496af7a9c97de82717adcf83d20bcff2a9f
E BOUTALEB <kagekyio@gmail.com>
2015-10-18 13:15:58 +0200

    Basic authentication scheme

--- app.js
+++ app.js
@@ -1,12 +1,46 @@
 var express = require('express');
-var app = express();
+var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
 var wineController = require('./routes/rest');
-var routes = require('./routes/index'); 
+var routes = require('./routes/index');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(require('express').static(__dirname + '/public'));
 
 app.use('/', routes);
 app.use('/api', wineController);
+
+// passport config
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+// mongoose
+mongoose.connect('mongodb://localhost/users');
 
 module.exports = app;
