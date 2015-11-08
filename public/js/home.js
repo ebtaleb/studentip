33993b1e16ee86e4c117db7df2d3f04103150976
Alexandre Annic <alexandre.annic1@gmail.com>
2015-11-08 16:05:01 +0100

    New interface

--- public/js/home.js
+++ public/js/home.js
@@ -1,18 +1,22 @@
 var sendTip = function(username) {
-    text = $("textarea#sendie").val();
+    text = $("#sentTip-input").val();
     if (text) {
+        var alert = alert_new_loading("Envoi...");
 	    $.ajax({
 		    type: "POST",
 		    url: "api/tips/",
 		    data: {'owner': username, 'content': text },
 		    dataType: "json",
 		    success: function(data){
-                noty({layout: 'bottom', type: 'success', text: "Astuce crée", timeout : 2000});
+                // noty({layout: 'bottom', type: 'success', text: "Astuce crée", timeout : 2000});
                 window.setTimeout( function() {window.location.href = data.url;}, 3000 ); 
+                alert.remove();
+                alert_new_success("Envoyé !");
 		    }
 	    });
     } else {
-        noty({layout: 'bottom', type: 'error', text: "Vous n'avez rien entré", timeout : 2000});
+        // noty({layout: 'bottom', type: 'error', text: "Vous n'avez rien entré", timeout : 2000});
+        alert_new_error("La zone de saisie est vide");
     }
 }
 
--- public/js/main.js
+++ public/js/main.js
@@ -4,22 +4,58 @@ function parseJsonDate(jsonDate) {
 };
 
 var createBubble = function(item) {
-
+    console.log(item._id);
     newdiv = document.createElement("div");
     newdiv.className = "tip";
 
-    newquote = document.createElement("blockquote");
-    newquote.className = "example-right";
+    // Create tip
+    newdiv.innerHTML = 
+        // "<a href='/" + item._id + "'>" +
+        "<div class='tip-container'>" +
+            "<div class='tip-img'><i class='fa fa-user'></i></div>" +
+            "<div class='tip-content'>" +
+                "<div class='tip-head'>" +
+                    "<div class='tip-author'>" + item.owner + "</div>" +
+                    "<div class='tip-date'>" + parseJsonDate(item.creation_date) + "</div>" +
+                "</div>" + 
+                "<div class='tip-txt'>" +
+                    item.content +
+                "</div>" +
+                "<div class='tip-actions'>" +
+                    "<i class='fa fa-comment' onclick=\"$('#tip-comments" + item._id +"').toggle()\"></i> " + item.comments.length +
+                "</div>" +
+            "</div>" +  
+        "</div>";
+        // "</a>";
 
-    newlink = document.createElement("a");
-    newlink.setAttribute("href", "/"+item._id);
-    newlink.innerHTML = item.content;
-    newquote.appendChild(newlink);
-    newdiv.appendChild(newquote);
+    // Prepare comments
+    var comments = "";
 
-    p_user = document.createElement("p");
-    p_user.innerHTML = item.owner + " - " + parseJsonDate(item.creation_date);
-    newdiv.appendChild(p_user);
+    for(var i in item.comments) {
+       comments += 
+            "<div class='tip-comment'>" +
+                "<div class='tip-com-img'><i class='fa fa-user'></i></div>" +
+                "<div class='tip-com-content'>" +
+                    "<div class='tip-com-head'>" +
+                        "<div class='tip-com-author'>" + item.comments[i].owner + "</div>" +
+                        "<div class='tip-com-date'>" + parseJsonDate(item.comments[i].creation_date) + "</div>" +
+                    "</div>" + 
+                    "<div class='tip-com-txt'>" +
+                        item.comments[i].content +
+                    "</div>" +
+                "</div>" +  
+            "</div>";
+    }
+
+    // Add comments under tip
+    newdiv.innerHTML += 
+        "<div id='tip-comments" + item._id + "' class='tip-comments' style='display:none'>" +
+            comments + 
+            "<div class='comment-add'>" +
+                "<textarea id='comment-add-txt" + item._id + "' class='comment-add-txt'></textarea>" +
+                "<i class='fa fa-paper-plane' onclick=\"sendComment($('#comment-add-txt" + item._id + "').val(), '" + item._id+"')\"></i>" +
+            "</div>" + 
+        "</div>";
 
     return newdiv;
 };
@@ -30,5 +66,32 @@ $(document).ready(function () {
             $.each(data, function (key, item) {
                 $(".container.new-tips").append(createBubble(item));
             });
+
+            if($("#user-offline").val() == 1) {
+                $(".comment-add").remove();
+            }
         });
 });
+
+var sendTip = function(username) {
+    text = $("#sentTip-input").val();
+    if (text) {
+        var alert = alert_new_loading("Envoi...");
+        $.ajax({
+            type: "POST",
+            url: "api/tips/",
+            data: {'owner': username, 'content': text },
+            dataType: "json",
+            success: function(data){
+                // noty({layout: 'bottom', type: 'success', text: "Astuce crée", timeout : 2000});
+                window.setTimeout( function() {window.location.href = data.url;}, 3000 ); 
+                alert.remove();
+                alert_new_success("Envoyé !");
+                $("#sentTip-input").val("");
+            }
+        });
+    } else {
+        // noty({layout: 'bottom', type: 'error', text: "Vous n'avez rien entré", timeout : 2000});
+        alert_new_error("La zone de saisie est vide");
+    }
+}
